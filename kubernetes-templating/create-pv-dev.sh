77de6b250ee63b9c466eaf87ba2507a3c736ce2d
Voronova Anastasiya <bastetnvv@gmail.com>
2025-10-31 16:22:01 +0000

    Add kubernetes-templating

---
+++ kubernetes-templating/create-pv-dev.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for i in {0..0}; do
+  kubectl apply -f - <<EOF
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: nfs-kafka-dev-pv-${i}
+  namespace: dev
+spec:
+  capacity:
+    storage: 3Gi
+  volumeMode: Filesystem
+  accessModes:
+    - ReadWriteMany
+  persistentVolumeReclaimPolicy: Retain
+  storageClassName: nfs-storage
+  nfs:
+    server: 192.168.1.59
+    path: "/data/kafka/dev/broker-${i}"
+    readOnly: false
+EOF
+done
